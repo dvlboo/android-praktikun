ba108ba39eacd5cbd5bf2345ff76ed6a0c29a8ea
dvlboo <kuhhhckr@gmail.com>
2023-04-26 23:12:12 +0700

    explicit intent done, implicit intent (bug)

--- Modul3/INTENT/app/src/main/java/com/example/aplikasipembayaranonline/MainActivity.kt
+++ Modul3/INTENT/app/src/main/java/com/example/aplikasipembayaranonline/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.aplikasipembayaranonline
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.aplikasipembayaranonline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cvTentangAplikasi.setOnClickListener(
+            View.OnClickListener {
+                Intent(this,AboutActivity::class.java).also { startActivity(it) }
+            }
+        )
+
+//        val packageName = "com.gojek.wallet"
+//        val scheme = "gopay://gopay"
+//        val uri = Uri.parse(scheme)
+        binding.cvPindahPembayaran.setOnClickListener(
+            View.OnClickListener {
+                val intent = Intent(Intent.CATEGORY_APP_MAPS)
+//                intent.`package` = packageName
+                startActivity(intent)
+            }
+        )
     }
 }
